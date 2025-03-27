3943c48adec48fcacd52be013c338c0d1ee77aba
mireuryu <126295813+mireuryu@users.noreply.github.com>
2025-03-27 16:28:59 -0500

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
 namespace GithubActionsLab;
 
 public class Program
@@ -86,6 +87,6 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{
-		return 0.0;
+		return Math.Pow(double.Parse(x), double.Parse(y));
 	}
 }
